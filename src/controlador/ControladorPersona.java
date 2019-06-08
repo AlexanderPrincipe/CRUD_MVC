b5c80095bb41cdcd3e3547166d86fac8d500e212
Alexander Principe <principetolentinoa@hotmail.com>
2019-06-07 19:20:40 -0500

    CRUD 100%

--- src/controlador/ControladorPersona.java
+++ src/controlador/ControladorPersona.java
@@ -21,6 +21,9 @@ public class ControladorPersona implements ActionListener{
         this.modelo = modelo;
         vista.botonInsertar.addActionListener(this);
         vista.botonLimpiar.addActionListener(this);
+        vista.botonBuscar.addActionListener(this);
+        vista.botonModificar.addActionListener(this);
+        vista.botonEliminar.addActionListener(this);
     }
     
     public void iniciar(){
@@ -51,10 +54,58 @@ public class ControladorPersona implements ActionListener{
         if (ae.getSource() == vista.botonLimpiar) {
             limpiarCajas();
         }
+        if (ae.getSource() == vista.botonBuscar) {
+            persona.setClave(vista.cajaBuscar.getText());
+            
+            if (modelo.buscar(persona)) {
+                
+                vista.cajaID.setText(String.valueOf(persona.getIdPersona()));
+                vista.cajaClave.setText(persona.getClave());
+                vista.cajaNombre.setText(persona.getNombre());
+                vista.cajaDomicilio.setText(persona.getDomicilio());
+                vista.cajaCelular.setText(persona.getCelular());
+                vista.cajaCorreo.setText(persona.getCorreo());
+                vista.comboGenero.setSelectedItem(persona.getGenero());    
+            }else {
+                JOptionPane.showMessageDialog(null, "No existe una persona con esa clave");
+                limpiarCajas();
+            }
+        }
+        
+        if (ae.getSource() == vista.botonModificar) {
+            persona.setIdPersona(Integer.parseInt(vista.cajaID.getText()));
+            persona.setClave(vista.cajaClave.getText());
+            persona.setNombre(vista.cajaNombre.getText());
+            persona.setDomicilio(vista.cajaDomicilio.getText());
+            persona.setCelular(vista.cajaCelular.getText());
+            persona.setCorreo(vista.cajaCorreo.getText());
+            persona.setGenero(vista.comboGenero.getSelectedItem().toString());
+            
+            if (modelo.modificar(persona)) {
+                JOptionPane.showMessageDialog(null, "Registro modificado correctamente");
+                limpiarCajas();
+            }else {
+                JOptionPane.showMessageDialog(null, "No se pudo modificar el registro");
+                limpiarCajas();
+            }
+        }
+        
+        if (ae.getSource() == vista.botonEliminar) {
+            persona.setIdPersona(Integer.parseInt(vista.cajaID.getText()));
+            
+            if (modelo.eliminar(persona)) {
+                JOptionPane.showMessageDialog(null, "Registro eliminado correctamente");
+                limpiarCajas();
+            }else {
+                JOptionPane.showMessageDialog(null, "No se pudo eliminar el registro");
+                limpiarCajas();
+            }
+        }
     }
     
     
     public void limpiarCajas(){
+        vista.cajaID.setText(null);
         vista.cajaBuscar.setText(null);
         vista.cajaClave.setText(null);
         vista.cajaNombre.setText(null);
--- src/modelo/ConsultasPersona.java
+++ src/modelo/ConsultasPersona.java
@@ -47,5 +47,105 @@ public class ConsultasPersona extends Conexion{
         
     }
     
+    public boolean buscar(Persona persona){
+        Connection conexion = getConnection();
+        
+        try{
+            ps = conexion.prepareStatement("select * from persona where clave = ?");
+            ps.setString(1, persona.getClave());
+            rs = ps.executeQuery();
+           
+            if (rs.next()) {
+                persona.setIdPersona(rs.getInt("idPersona"));
+                persona.setClave(rs.getString("clave"));
+                persona.setNombre(rs.getString("nombre"));
+                persona.setDomicilio(rs.getString("domicilio"));
+                persona.setCelular(rs.getString("celular"));
+                persona.setCorreo(rs.getString("correo"));
+                persona.setGenero(rs.getString("genero"));
+                return true;
+            }else {
+                return false;
+            }
+            
+        }catch(Exception ex){
+            System.out.println("Error, " +ex);
+            return false;
+        }finally{
+            try{
+                conexion.close();
+            }catch(Exception ex){
+                System.out.println("Error, "+ex);
+            }
+        }
+    }
+    
+    
+    public boolean modificar(Persona persona){
+        Connection conexion = getConnection();
+        
+        try{
+            ps = conexion.prepareStatement("update persona set clave=?,nombre=?,domicilio=?,celular=?,correo=?,genero=? where idPersona=?");
+            ps.setString(1, persona.getClave());
+            ps.setString(2, persona.getNombre());
+            ps.setString(3, persona.getDomicilio());
+            ps.setString(4, persona.getCelular());
+            ps.setString(5, persona.getCorreo());
+            ps.setString(6, persona.getGenero());
+            ps.setInt(7, persona.getIdPersona());
+            
+            int resultado = ps.executeUpdate();
+            
+            if (resultado > 0) {
+                return true;  
+            }else {
+                return false;
+            }
+            
+            
+        }catch(Exception ex){
+            System.out.println("Error, " +ex);
+            return false;
+        }finally{
+            try{
+                conexion.close();
+            }catch(Exception ex){
+                System.out.println("Error, "+ex);
+            }
+        }
+        
+        
+    }
+    
+    public boolean eliminar(Persona persona){
+        Connection conexion = getConnection();
+        
+        try{
+            ps = conexion.prepareStatement("delete from persona where idPersona=?");
+            ps.setInt(1, persona.getIdPersona());
+            
+            int resultado = ps.executeUpdate();
+            
+            if (resultado > 0) {
+                return true;  
+            }else {
+                return false;
+            }
+            
+            
+        }catch(Exception ex){
+            System.out.println("Error, " +ex);
+            return false;
+        }finally{
+            try{
+                conexion.close();
+            }catch(Exception ex){
+                System.out.println("Error, "+ex);
+            }
+        }
+        
+        
+    }
+    
     
 }
